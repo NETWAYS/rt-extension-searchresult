a28055e475a9df70722eeb55a9d09416d5e948c9
Michael Friedrich <michael.friedrich@netways.de>
2018-12-07 16:27:53 +0100

    Release v1.0.0

--- lib/RT/Extension/SearchResult.pm
+++ lib/RT/Extension/SearchResult.pm
@@ -5,7 +5,7 @@ use strict;
 use version;
 use RT;
 
-our $VERSION='0.9.0';
+our $VERSION='1.0.0';
 
 if (eval { require RT::Extension::FontAwesome; }) {
     RT->Logger->debug('SearchResults: RT-Extension-FontAwesome is available, omit loading');
